bfad4ba6397bc836c99c63837c6f92623d883373
Ruslan Golyautdinov <golyautdinov.r@yandex.ru>
2024-05-22 14:08:39 +0300

    sprint 4 review

--- src/test/java/basicQATest/Base_Test.java
+++ src/test/java/basicQATest/Base_Test.java
@@ -4,6 +4,7 @@ import org.junit.After;
 import org.junit.Before;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import static pages.BasicPage.BASE_URI;
 
 public class Base_Test {
@@ -13,7 +14,10 @@ public class Base_Test {
     @Before
     public void startUp() {
         System.setProperty("chromedriver", "./src/main/java/resources/webdriver/chrome/chromedriver");
+        System.setProperty("geckodriver", "./src/main/java/resources/webdriver/firefox/geckodriver");
+        // выбор браузера для прогона тестов:
         driver = new ChromeDriver();
+        //driver = new FirefoxDriver();
         driver.manage().window().maximize();
         driver.get(BASE_URI);
     }
